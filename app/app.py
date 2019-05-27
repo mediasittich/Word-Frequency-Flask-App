292db2bb4961bac20a20a248ab6395fa8c52ff9a
Regina Galambos <gina.galambos@gmail.com>
2019-05-27 03:18:01 +0200

    add templates folder and index.html file

--- app/app.py
+++ app/app.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello from Flask'
+    var = 'Hello from FLASK!'
+    return render_template('index.html', var=var)
