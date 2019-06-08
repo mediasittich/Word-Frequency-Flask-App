e8fba3540b39a30c16c4f247320d060aef63a57d
Regina Galambos <gina.galambos@gmail.com>
2019-06-09 01:39:14 +0200

    install additional libraries and add scripts for text processing, include plot.ly js into HTML, update templates, add Bootstrap

--- app/app.py
+++ app/app.py
@@ -1,4 +1,9 @@
 import os
+import nltk
+import pandas as pd
+import plotly
+import plotly.graph_objs as go
+import json
 from flask import Flask, render_template, request, redirect, url_for, flash
 from werkzeug.utils import secure_filename
 
@@ -12,6 +17,18 @@ app.config['SECRET_KEY'] = 'dev'
 def allowed_file(filename):
     return '.' in filename and \
         filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def create_plot(x_list,y_list):
+    data = [
+        go.Bar(
+            x=x_list,
+            y=y_list
+        )
+    ]
+
+    graphJSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
+
+    return graphJSON
 
 @app.route('/', methods = ['GET', 'POST'])
 def index():
@@ -45,7 +62,38 @@ def index():
 
 @app.route('/<file>')
 def show_stats(file):
-    return render_template('textspecs.html', file=file)
+    # load text
+    f = open(UPLOAD_FOLDER + '/' + file + '.txt', 'r')
+    raw = f.read()
+
+    # text processing
+    tokenizer = nltk.tokenize.RegexpTokenizer('\w+')
+    tokens = tokenizer.tokenize(raw)
+
+    words = []
+
+    for word in tokens:
+        words.append(word.lower())
+    
+    sw = nltk.corpus.stopwords.words('english')
+    
+    words_ns = []
+
+    for word in words:
+        if word not in sw:
+            words_ns.append(word)
+    
+    freqdist = nltk.FreqDist(words_ns)
+    freqdist.most_common(25)
+
+    df_fdist = pd.DataFrame.from_dict(freqdist, orient='index')
+    df_fdist = df_fdist.reset_index()
+    df_fdist.columns = ['Word', 'Frequency']
+    df_fdist = df_fdist.sort_values(by='Frequency', ascending=False)
+
+    bar = create_plot(df_fdist['Word'][:25], df_fdist['Frequency'][:25])
+
+    return render_template('textspecs.html',file=file, sorted_words=freqdist.most_common(25), plot=bar)
 
 # TODO: on submit display 'file uploaded successfully' or 'no file selected/invalid file'
 # TODO: when file upload success, display filename and a button 'view text analysis'
